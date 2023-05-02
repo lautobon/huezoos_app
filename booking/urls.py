ec0873ea1725f21f2b4eb387056db0255714d3b3
Laura Tobon <lauranataliatobon@gmail.com>
2023-05-01 22:06:53 -0500

    Autenticacion

--- booking/urls.py
+++ booking/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path  
-from .views import home, register_form
+from .views import home, register_form, login_huezoos, auth_home
 
 urlpatterns = [
     path('', home, name='index'),
-    path('register', register_form, name='register')
+    path('home', auth_home, name='home'),
+    path('register', register_form, name='register'),
+    path('login', login_huezoos, name='login')
 ]
--- booking/views.py
+++ booking/views.py
@@ -2,24 +2,50 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import RegisterForm
 from django.shortcuts import redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+
 def home(request):
     return HttpResponse('joli')
+
+@login_required
+def auth_home(request):
+    user = request.user
+    context = {}
+    if user.is_authenticated:
+        context['msn'] = 'Autenticado'
+
+    return render(request, 'index.html', context)
+
+def login_huezoos(request):
+    if request.method == 'POST':
+        email = request.POST["email"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        
+        return redirect('index')
+
+    else:
+        return render(request, 'login.html', {})
 
 def register_form(request):
 
     if request.method == 'POST':
         form = RegisterForm(request.POST)
-        print(form.is_valid)
         if form.is_valid():
 
             user = form.save()
-            print(user)
+
             login(request, user)
-            return redirect('index')
+            return redirect('home')
     else:
 
         form = RegisterForm()
